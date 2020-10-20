8a28cb959ca593abd4bb50b18507ba278491b123
alai031 <alai031@ucr.edu>
2020-10-20 16:23:09 -0700

    Moved the five files

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include "c-echo.h"
+
+#include "gtest/gtest.h"
+
+TEST(EchoTest, HelloWorld) {
+	char* test_val[3];
+	test_val[0] = "./c-echo";
+	test_val[1] = "hello";
+	test_val[2] = "world";
+	EXPECT_EQ("hello world", echo(3, test_val));
+}
+
+TEST(EchoTest, EmptyString) {
+	char* test_val[1];
+	test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, CapitalLetters) {
+	char* test_val[3];
+	test_val[0] = "./c-echo";
+	test_val[1] = "PETER";
+	test_val[2] = "PAN";
+	EXPECT_EQ("PETER PAN", echo(3, test_val));
+}	
+
+TEST(EchoTest, SpecialCharacters) {
+	char* test_val[3];
+	test_val[0] = "./c-echo";
+	test_val[1] = "%$#^";
+	test_val[2] = "*!#^";
+	EXPECT_EQ("%$#^ *!#^", echo(3, test_val));
+}
+
+TEST(EchoTest, Numbers) {
+	char* test_val[4];
+	test_val[0] = "./c-echo";
+	test_val[1] = "42";
+	test_val[2] = "32";
+	test_val[3] = "100";
+	EXPECT_EQ("42 32 100", echo(4, test_val));
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
